690a124be95d1395f02c187f5544bc3f4bab4d1d
wakhu shibero <wshibero@gmail.com>
2024-05-30 12:44:47 +0000

    created bus and vehicle class

--- main.rb
+++ main.rb
@@ -1,1 +1,24 @@
 puts 'Hello, World!'
+
+class Vehicle
+  def initialize(manufacturer, year, model, price)
+    @manufacturer = manufacturer
+    @year = year
+    @model = model
+    @price = price
+  end
+  def string()
+    return "Manufacturer: #{@manufacturer}\nYear: #{@year}\nModel: #{@model}\nPrice: #{@price}"
+  end
+end
+
+class Bus < Vehicle
+  def initialize(manufacturer, year, model, price, seats)
+    super(manufacturer, year, model, price)
+    @seats = seats
+  end
+end
+
+merc = Vehicle.new("Mercedes", "2020", "S-Class", 1200000)
+puts merc.string()
+  
